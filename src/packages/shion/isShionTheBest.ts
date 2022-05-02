d64434412dff4d19838e710dca06c9df29cc664c
José Silva Oliveira <jose.silva.oliveira@devoteam.com>
2022-05-02 16:04:57 +0100

    feat(monorepo): monorepo

---
+++ src/packages/shion/isShionTheBest.ts
@@ -0,0 +1,5 @@
+const isShionTheBest = () => true
+
+export {
+	isShionTheBest
+}
---
+++ src/packages/shion/test/isShionTheBest.test.ts
@@ -0,0 +1,6 @@
+import { isShionTheBest } from "../isShionTheBest"
+import 'jest'
+
+it('isShionTheBest', () => {
+	expect(isShionTheBest()).toBeTruthy();
+})
